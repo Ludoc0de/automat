4d3335d04f7300177b45062c7c5fb2afde4c1606
ludoc0de <champi.ludo@tutanota.com>
2024-08-18 23:40:51 -0300

    add headers

--- log.py
+++ log.py
@@ -1,10 +1,7 @@
-import sys
 import requests
 from bs4 import BeautifulSoup
 
-sys.stdout.reconfigure(encoding='utf-8')
-
-login_url = ("https://b12.io/accounts/login/?next=/dashboard/")
+login_url = ("https://b12.io/dashboard/")
 # dashboard_url = ("")
 
 # Création d'une session pour conserver les cookies
@@ -14,13 +11,21 @@ session = requests.Session()
 response = session.get(login_url)
 soup = BeautifulSoup(response.text, 'html.parser')
 
+# Rechercher le token CSRF (le nom du champ peut varier en fonction du site)
+csrf_token = soup.find('input', {'name': 'csrfmiddlewaretoken'})['value']
+
+headers = {
+    "Referer": login_url,
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+}
+
 payload = {
     "username": "config.EMAIL",
     "password": "config.PASSWORD",
-    "csrfmiddlewaretoken": "config.TOKENS"
+    "csrfmiddlewaretoken": csrf_token
 }
 
-login_response = session.post(login_url, data=payload)
+login_response = session.post(login_url, data=payload, headers=headers)
 print(login_response.text)
 # requests_get = requests.get(dashboard_url)
 # print(requests_get.text)
