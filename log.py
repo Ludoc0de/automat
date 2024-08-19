7087e72d33014b5b08e7df6ebd1bac8b337b3d96
ludoc0de <champi.ludo@tutanota.com>
2024-08-19 01:06:09 -0300

    verification

--- log.py
+++ log.py
@@ -1,18 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
-login_url = ("https://b12.io/dashboard/")
-# dashboard_url = ("")
+login_url = ("https://frontendmasters.com/login/")
 
-# Création d'une session pour conserver les cookies
+# # Création d'une session pour conserver les cookies
 session = requests.Session()
 
-# Obtenir la page de connexion pour récupérer les cookies et éventuellement des tokens CSRF
+# # Obtenir la page de connexion pour récupérer les cookies et éventuellement des tokens CSRF
 response = session.get(login_url)
 soup = BeautifulSoup(response.text, 'html.parser')
 
-# Rechercher le token CSRF (le nom du champ peut varier en fonction du site)
-csrf_token = soup.find('input', {'name': 'csrfmiddlewaretoken'})['value']
+# # Rechercher le token CSRF (le nom du champ peut varier en fonction du site)
+# csrf_token = soup.find('input', {'name': 'csrfmiddlewaretoken'})['value']
 
 headers = {
     "Referer": login_url,
@@ -20,12 +19,26 @@ headers = {
 }
 
 payload = {
-    "username": "config.EMAIL",
-    "password": "config.PASSWORD",
-    "csrfmiddlewaretoken": csrf_token
+    "email": "config.EMAIL",
+    "password": "config.PASSWORD"
 }
 
 login_response = session.post(login_url, data=payload, headers=headers)
-print(login_response.text)
-# requests_get = requests.get(dashboard_url)
-# print(requests_get.text)
+# print(login_response.text)
+
+dashboard_url = ("https://frontendmasters.com/dashboard/")
+dashboard_response = session.get(dashboard_url)
+print("dashboard")
+print(dashboard_response.text)
+
+# # Vérifier si un cookie spécifique est présent après la connexion
+# if "sessionid" in session.cookies:
+#     print("Connexion réussie, session active")
+# else:
+#     print("Échec de la connexion, aucun cookie de session trouvé")
+
+# # Vérifier si vous êtes redirigé vers la page de connexion ou si le tableau de bord est affiché
+# if "Log out" in dashboard_response.text or "Tableau de bord" in dashboard_response.text:
+#     print("Connexion réussie, accès au tableau de bord")
+# else:
+#     print("Échec de la connexion, redirection vers la page de connexion")
